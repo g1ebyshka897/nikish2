46ef015a77d37ffe1a22b2918cfa5dbe7925e6be
g1ebyshka897 <g1ebyshka897@icloud.com>
2025-02-27 18:38:13 +0000

    nikish

---
+++ src/main/java/app/Application.java
@@ -0,0 +1,19 @@
+package app;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Theme
+@PWA(name = "My Progressive Web Application",
+        shortName = "MyPWA")
+public class Application implements AppShellConfigurator {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
